89e26bcbfbdff8839ef4f37b685a8c40330fef47
RichardMarkRoss <cptrichkidprice@gmail.com>
2018-04-18 10:33:51 +0200

    commit

--- js/settings-bill.js
+++ js/settings-bill.js
@@ -17,32 +17,8 @@ var callAmount = 0;
 var smsAmount = 0;
 var warnLevel = 0;
 var critLevel = 0;
+var totalValue = 0
 
-function addSetting(){
-
-var updateAmountCall = callCostSetting.value;
-var updateAmountSms = smsCostSetting.value;
-var updateAmountcritLvl = criticalLevelSetting.value;
-var updateAmountwarnLvl = warningLevelSetting.value;
-
-if(callCostSetting != ""){
-    callAmount = parseFloat(updateAmountCall);
-    //callTheTotal += callAmount;
-  }
-
-  if(smsCostSetting != ""){
-      smsAmount = parseFloat(updateAmountSms);
-    //  smsTheTotal += smsAmount;
-    }
-  // console.log(callAmount)
-  // console.log(smsAmount)
-    if(warningLevelSetting != ""){
-    warnLevel = parseFloat(updateAmountwarnLvl);
-    }
-    if(criticalLevelSetting != ""){
-    critLevel = parseFloat(updateAmountcritLvl);
-    }
-}
 
 function addTotal(){
 
@@ -54,23 +30,22 @@ function addTotal(){
 
     if (billItemSet === "call"){
         callTheTotal += callAmount;
+        if(callTheTotal > critLevel ){
+          var diff = callTheTotal - critLevel;
+          callTheTotal -= diff
+
+        }
+     }else if (billItemSet === "sms"){
+         smsTheTotal += smsAmount;
+         if (smsTheTotal > critLevel){
+           var diff = smsTheTotal - critLevel;
+          smsTheTotal -= diff;
+         }
     }
-
-    if (billItemSet === "sms"){
-        smsTheTotal += smsAmount;
-    }
-
-
-
-    // if (billItemWithSetting = smsAmount){
-    //     smsTotalsetting += smsAmount;
-    // }
-    console.log(callTheTotal);
-
 
     smsTotalsetting.innerHTML = smsTheTotal.toFixed(2);
     callTotalSetting.innerHTML = callTheTotal.toFixed(2);
-    var totalValue = callTheTotal + smsTheTotal;
+     totalValue = callTheTotal + smsTheTotal;
     totalSetting.innerHTML = totalValue.toFixed(2);
 
     if (totalValue >= critLevel){
@@ -78,11 +53,47 @@ function addTotal(){
 
     }
 
+
+
     if (totalValue >= warnLevel){
         totalSetting.classList.add("warning");
     }
 
 	}
 
+  function addSetting(){
+
+  var updateAmountCall = callCostSetting.value;
+  var updateAmountSms = smsCostSetting.value;
+  var updateAmountcritLvl = criticalLevelSetting.value;
+  var updateAmountwarnLvl = warningLevelSetting.value;
+
+  if(callCostSetting != ""){
+      callAmount = parseFloat(updateAmountCall);
+    }
+
+    if(smsCostSetting != ""){
+        smsAmount = parseFloat(updateAmountSms);
+      }
+    // console.log(callAmount)
+    // console.log(smsAmount)
+      if(warningLevelSetting != ""){
+      warnLevel = parseFloat(updateAmountwarnLvl);
+      }
+      if(criticalLevelSetting != ""){
+      critLevel = parseFloat(updateAmountcritLvl);
+      }
+
+      if (critLevel > totalValue){
+          totalSetting.classList.remove("danger");
+
+      }
+
+
+      // if (warnLevel > totalValue){
+      //     totalSetting.classList.remove("warning");
+      //
+      //       }
+  }
+  buttonAdd.addEventListener("click", addTotal);
   updateSettingBtn.addEventListener("click", addSetting);
-    buttonAdd.addEventListener("click", addTotal);
